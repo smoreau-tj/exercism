3a68e1cad04fd6adca3fdb901c9e83c3096c9dc4
serge-tj <serge@tommyjohn.com>
2018-04-18 15:15:05 -0400

    Completed hamming test

---
+++ hamming/hamming.rb
@@ -0,0 +1,19 @@
+module BookKeeping
+  VERSION = 3
+end
+class Hamming
+
+  def self.compute(original, mutation)
+    if (original.length != mutation.length)
+      raise ArgumentError
+    end
+    difference = 0
+    mutation_split = mutation.split(//)
+    original.split('').each_with_index do |char, index|
+      unless mutation_split[index] == char
+        difference = difference + 1
+      end
+    end
+    difference
+  end
+end
