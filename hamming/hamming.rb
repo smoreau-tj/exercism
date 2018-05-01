59151e2947c7ec3bc4bf52d0923765eddef85880
serge-tj <serge@tommyjohn.com>
2018-05-01 17:00:53 -0400

    refactored code so that it is more legible and eliminated code smell

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -4,16 +4,12 @@ end
 class Hamming
 
   def self.compute(original, mutation)
-    if original.length != mutation.length
-      raise ArgumentError
-    end
-    difference = 0
-    mutation_split = mutation.split(//)
-    original.split('').each_with_index do |char, index|
-      unless mutation_split[index] == char
-        difference +=  1
-      end
-    end
-    difference
+    raise ArgumentError unless original.length == mutation.length
+
+    original.chars.zip(mutation.chars).count { |original_char, mutation_char| original_char != mutation_char }
+    # original.chars.each_with_index.count do |char, index|
+    #   mutation.chars[index] != char
+    # end
+
   end
 end
